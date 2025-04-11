1af60629cbaf81c80ac95e6d232c95fe6499f342
lionel754 <2416552069@qq.com>
2025-04-11 14:27:03 +0800

    update
    问题

--- 2D project/Assets/Scripts/Enemy/Enemy.cs
+++ 2D project/Assets/Scripts/Enemy/Enemy.cs
@@ -18,6 +18,17 @@ public class Enemy : MonoBehaviour
     public float currentSpeed;
 
     public Vector3 faceDir;
+    public float hurtForce;
+    public Transform attacker;
+
+    [Header("¼ÆÊ±Æ÷")]
+    public float waitTime;
+    public float waitTimeCounter;
+    public bool wait;
+    [Header("×´Ì¬")]
+    public bool isHurt;
+
+    public bool isDead;
 
     private void Awake()
     {
@@ -25,26 +36,75 @@ public class Enemy : MonoBehaviour
         anim = GetComponent<Animator>();
         physicsCheck = GetComponent<PhysicsCheck>();
 
+
         currentSpeed = normalSpeed;
+        waitTimeCounter = waitTime;
     }
 
     private void Update()
     {
         faceDir = new Vector3(-transform.localScale.x, 0, 0);
-        if(physicsCheck.touchLeftWall||physicsCheck.touchRightWall)
+        if ((physicsCheck.touchLeftWall&&faceDir.x<0) || (physicsCheck.touchRightWall&&faceDir.x>0))
         {
-            transform.localScale = new Vector3(faceDir.x, 1, 1);
+            wait = true;
+            anim.SetBool("walk", false);
         }
-
+        TimeCounter();
     }
     private void FixedUpdate()
     {
-        Move();
+        if(!isHurt&isDead)
+            Move();
 
     }
     public virtual void Move()
     {
         rb.velocity = new Vector2(currentSpeed * faceDir.x * Time.deltaTime, rb.velocity.y);
 
+    }
+
+    public void TimeCounter()
+    {
+        if(wait)
+        {
+            waitTimeCounter -= Time.deltaTime;
+            if(waitTimeCounter<=0)
+            {
+                wait = false;
+                waitTimeCounter = waitTime;
+                transform.localScale = new Vector3(faceDir.x, 1, 1);
+            }
+        }
+    }
+    public  void    OnnTakeDamage(Transform attackTrans)
+    {
+        attacker =attackTrans;
+        if (attackTrans.position.x - transform.position.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        if (attackTrans.position.x - transform.position.x < 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        isHurt = true;
+        anim.SetTrigger("hurt");
+        Vector2 dir = new Vector2(transform.position.x - attackTrans.position.x, 0).normalized;
+
+        StartCoroutine(OnHurt(dir));
+        
+
+    }
+    private IEnumerator OnHurt(Vector2  dir)
+    {
+        rb.AddForce(dir * hurtForce, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.45f);
+        isHurt = false;
+    }
+    public void OnDie()
+    {
+        gameObject.layer = 2;
+        anim.SetBool("dead", true);
+        isDead = true;
+    }
+    public void DestroyAfterAnimation ()
+    {
+        Destroy(this.gameObject);
     }
 }
